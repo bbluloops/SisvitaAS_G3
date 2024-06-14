1430b84ff26c81631240c356b8f5e945e34a3810
bbluloops <nehisbell.terrones12@gmail.com>
2024-06-14 00:01:39 -0500

    commit inicial

---
+++ app/src/main/java/com/example/proyectosisvitag3/ui/theme/login/iu/LoginScreen.kt
@@ -0,0 +1,174 @@
+package com.example.proyectosisvitag3.ui.theme.login.iu
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Surface
+import androidx.compose.material3.TextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import com.example.proyectosisvitag3.R
+import androidx.compose.ui.unit.sp
+import androidx.compose.runtime.*
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LoginScreen(viewModel: LoginViewModel) {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Color.White
+    ) {
+        Box(
+            Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            Login(Modifier.align(Alignment.Center), viewModel)
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Login(modifier: Modifier, viewModel: LoginViewModel) {
+
+    val email :String by viewModel.email.observeAsState(initial = "")
+    val password :String by viewModel.password.observeAsState(initial = "")
+    val loginEnable:Boolean by viewModel.loginEnable.observeAsState(initial = false)
+    val coroutineScope = rememberCoroutineScope()
+
+    val isLoading:Boolean by viewModel.isLoading.observeAsState(initial = false)
+
+    if(isLoading){
+        Box(Modifier.fillMaxSize()){
+            CircularProgressIndicator(Modifier.align(Alignment.Center))
+        }
+    } else{
+        Column(modifier = modifier) {
+            HeaderImage(
+                Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .size(200.dp))
+            Spacer(modifier = Modifier.padding(16.dp))
+            EmailField(email) { viewModel.onLoginChanged(it, password) }
+            Spacer(modifier = Modifier.padding(4.dp))
+            PasswordField(password){viewModel.onLoginChanged(email,it)}
+            Spacer(modifier = Modifier.padding(8.dp))
+            ForgotPassword(Modifier.align(Alignment.End))
+            Spacer(modifier = Modifier.padding(16.dp))
+            LoginButton(loginEnable){
+                coroutineScope.launch {
+                    viewModel.onLoginSelected()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun LoginButton(loginEnable: Boolean, onLoginSelected: () -> Unit) {
+    Button(
+        onClick = { onLoginSelected()},
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(48.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color(0xFFFF4303),
+            disabledContainerColor = Color(0xFFF78058),
+            contentColor = Color.White,
+            disabledContentColor = Color.White
+        ), enabled = loginEnable
+        ) {
+            Text(text = "Iniciar sesión")
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ForgotPassword(modifier: Modifier){
+    Text(
+        text = "¿Olvidaste la contraseña?",
+        modifier = modifier.clickable { },
+        fontSize = 12.sp,
+        fontWeight = FontWeight.Bold,
+        color = Color(0xFFFB9600)
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PasswordField(password:String, onTextFieldChanged:(String) -> Unit){
+    TextField(
+    value = password, onValueChange ={onTextFieldChanged(it)},
+    placeholder = { Text(text = "Contraseña") },
+    modifier = Modifier.fillMaxWidth(),
+    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+    singleLine = true,
+    maxLines = 1,
+    colors = TextFieldDefaults.textFieldColors(
+        containerColor = Color(0xFFDEDDDD),
+        cursorColor = Color(0xFF636262),
+        focusedIndicatorColor = Color.Transparent,
+        unfocusedIndicatorColor = Color.Transparent,
+        focusedTextColor = Color(0xFF000000),
+        unfocusedTextColor = Color(0xFF000000)  // Texto en un tono más oscuro
+        )
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EmailField(email:String, onTextFieldChanged:(String) -> Unit) {
+    TextField(
+        value = email, onValueChange = { onTextFieldChanged(it) },
+        modifier = Modifier.fillMaxWidth(),
+        placeholder = { Text(text = "Email") },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+        singleLine = true,
+        maxLines = 1,
+        colors = TextFieldDefaults.textFieldColors(
+            containerColor = Color(0xFFDEDDDD),
+            cursorColor = Color(0xFF636262),
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+            focusedTextColor = Color(0xFF000000),
+            unfocusedTextColor = Color(0xFF000000)  // Texto en un tono más oscuro
+
+        )
+    )
+}
+
+@Composable
+fun HeaderImage(modifier: Modifier) {
+    Image(
+        painter = painterResource(id = R.drawable.logo_sisvita),
+        contentDescription = "Header",
+        modifier = modifier
+    )
+}
+
+
+
