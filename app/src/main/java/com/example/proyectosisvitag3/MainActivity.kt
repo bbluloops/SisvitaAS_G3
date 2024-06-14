eb609411f98628ebc4d67aeb915c99e9162c8b34
bbluloops <nehisbell.terrones12@gmail.com>
2024-06-14 00:01:39 -0500

    commit inicial

---
+++ app/src/main/java/com/example/proyectosisvitag3/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.proyectosisvitag3
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.proyectosisvitag3.ui.theme.ProyectoSisvitaG3Theme
+import com.example.proyectosisvitag3.ui.theme.login.iu.LoginScreen
+import com.example.proyectosisvitag3.ui.theme.login.iu.LoginViewModel
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ProyectoSisvitaG3Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    LoginScreen(LoginViewModel() )
+                }
+            }
+        }
+    }
+}
+
