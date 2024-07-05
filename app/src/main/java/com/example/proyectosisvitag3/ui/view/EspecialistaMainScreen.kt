15083e5a861bc0c86b7c8b77e16b545bec67cd88
DairFernandez28 <dairfernandez28@gmail.com>
2024-07-05 09:29:12 -0500

    Error especialistaMainScreen solucionado

--- app/src/main/java/com/example/proyectosisvitag3/ui/view/EspecialistaMainScreen.kt
+++ app/src/main/java/com/example/proyectosisvitag3/ui/view/EspecialistaMainScreen.kt
@@ -1,73 +1,31 @@
 package com.example.proyectosisvitag3.ui.theme.iu
 
-import android.util.Log
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.navigation.NavHostController
+import androidx.navigation.NavController
 
 @Composable
-fun EspecialistaMainScreen(navController: NavHostController) {
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = Color.White
+fun EspecialistaMainScreen(navController: NavController,idEspecialista: String) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .padding(16.dp),
     ) {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(
-                text = "Pantalla Principal del Especialista",
-                style = TextStyle(fontSize = 24.sp, color = Color.Black)
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-            Button(
-                onClick = {
-                    try{
-                        navController.navigate("especialistaTestScreen")
-                    }catch(e:Exception)
-                    {
-                        Log.e("EspecialistaTestScreen",e.toString())
-                    }
-                },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 32.dp)
-            ) {
-                Text(text = "Ver Tests Registrados")
-            }
-            Button(
-                onClick = {
-                    try{
-                        navController.navigate("mapaDeCalor")
-                    }catch(e:Exception)
-                    {
-                        Log.e("MapaDeCalor",e.toString())
-                    }
-                },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 32.dp)
-            ) {
-                Text(text = "Ver mapa de calor")
-            }
+        Text(text = "Pantalla principal del Especialista")
+        Button(onClick = {
+            navController.navigate("resultadosTestScreen/${idEspecialista}")
+        }){
+            Text(text= "Test Realizados")
         }
     }
 }
